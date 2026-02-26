fcd8cf541e89422dcc33157e5b5f4f2b308e0038
ana nurkaromah <ana.karomah@gmail.com>
2026-02-27 01:50:23 +0700

    02_terraform

---
+++ 02_terraform/2. terraform_with_variables/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/home/msimodern/DE_Zoomcamp2026_Theory/02_terraform/2. terraform_with_variables/keys/my_creds.json")
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
